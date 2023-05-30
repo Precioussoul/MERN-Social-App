daddeff65410650a1fd006277ce76ebabed591f4
Precioussoul <habsoff@gmail.com>
2023-05-30 22:55:36 +0100

    feat: added messenger component and set its routes

---
+++ client/src/pages/messenger/Messenger.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Topbar from "../../components/topbar/Topbar"
+
+const Messenger = () => {
+  return (
+    <>
+      <Topbar />
+      <div>Messenger</div>
+    </>
+  )
+}
+
+export default Messenger
