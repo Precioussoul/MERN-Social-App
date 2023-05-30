a9d7c4cf87480ea11ada0d7b9978558786c7a31a
Precioussoul <habsoff@gmail.com>
2023-05-30 23:11:38 +0100

    feat: connect conversation css file to Conversation component and gradually started styling

--- client/src/components/conversatiion/Conservation.jsx
+++ client/src/components/conversatiion/Conservation.jsx
@@ -1,7 +1,13 @@
 import React from "react"
+import "./conversation.css"
 
 const Conservation = () => {
-  return <div>Conservation</div>
+  return (
+    <div className='conversation'>
+      <img src='' alt='' className='conversationImg' />
+      <span className='conversationName'>John Doe</span>
+    </div>
+  )
 }
 
 export default Conservation
